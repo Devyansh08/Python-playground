9039fd038044fc61d20ac30e81efca076e2d9f58
Devyansh Karma <devyanshkarma.00@gmail.com>
2025-08-29 20:33:52 +0530

    basic

---
+++ basics/first-basic.py
@@ -0,0 +1,18 @@
+print("Devyansh karma")
+
+"""
+    Some small but key point 👇
+
+    1. we use # for single line comment and '''' or double  for multiline comment .
+
+    2.we use + to add two string manually and ','to add different data type mannually '
+"""
+# ----------------------------------------------------------------------------------------------
+
+name = "devyansh"
+age = 20
+price =0.7
+print(type(name))
+print(type(age))
+print(type(price))
+''' we can use type() in this to find the data type of the variables '''
