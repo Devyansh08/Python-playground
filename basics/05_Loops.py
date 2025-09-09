8d512f7e3dc477c5262e9846d7e4392a594a6643
Devyansh Karma <devyanshkarma.00@gmail.com>
2025-09-09 22:07:37 +0530

    loops

--- basics/05_Loops.py
+++ basics/05_Loops.py
@@ -33,9 +33,38 @@
 #     num+=1
 
 ''' continue--> continue terminate the current iteration and jump for the next one in the loop.'''
-num=0
-while num<5:
-    print(num)
-    if(num==3):
-        continue
-    num+=1
+# num=0
+# while num<5:
+#     print(num)
+#     if(num==3):
+#         num+=1
+#         continue
+#     num+=1
+
+
+# ------- For loop ----
+# list =[1,2,3,4,5]
+# for l in list:
+#     print(l)
+
+"for with else "
+# list =[1,2,3,4,5]
+# for l in list:
+#     print(l)
+# else:           # At the end when our whole loop runs the thing inside the else will print .
+#     print("End")
+
+# --- range()---
+''' Range return a sequence of number start from 0(by default)  and increased by 1(by default) and stops before a specific number.
+        range(start,stop,step)
+        and here stop value dena complusory hai.
+'''
+# for i in range(1,6): # here start is 1 and end 6 
+#     print(i) # here it start from 1 and end at n-1=5
+
+
+# --- pass ---
+''' Pass statement is used when we want do nothing in our loop and keep it for future purpose.'''
+# for i in range(5):
+#     pass 
+# print("some thing is remaing for future purpose.")
